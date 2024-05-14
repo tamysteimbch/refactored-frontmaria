56b43e2354b2a0a117071d12186341b40523fd03
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 20:04:55 -0300

    refactor: Update CreateExamBtn component to handle modal open and close actions

--- src/components/Sidebar/CreateExamBtn.tsx
+++ src/components/Sidebar/CreateExamBtn.tsx
@@ -1,20 +1,29 @@
 import { FaPlus } from 'react-icons/fa';
-import { Mode } from '@/constants/sidebuttons';
+import { useDisclosure } from '@chakra-ui/react';
+import Modal from '../Modal';
 
-interface CreateExamBtnProps {
-  setMode: (value: Mode) => void;
-}
-
-export default function CreateExamBtn({ setMode }: CreateExamBtnProps) {
+export default function CreateExamBtn() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <button
-      className="grid grid-cols-[auto,1fr] bg-secondary p-4 rounded-lg gap-4 w-4/6 mx-auto hover:bg-tertiary items-center"
-      onClick={() => setMode('newExam')}
-    >
-      <div className="text-xl">
-        <FaPlus />
-      </div>
-      <div className="text-md text-center">{'Criar nova prova'}</div>
-    </button>
+    <>
+      <button
+        className="grid grid-cols-[auto,1fr] bg-secondary p-4 rounded-lg gap-4 w-4/6 mx-auto hover:bg-tertiary items-center"
+        onClick={() => (isOpen ? onClose() : onOpen())}
+      >
+        <div className="text-xl">
+          <FaPlus />
+        </div>
+        <div className="text-md text-center">{'Criar nova prova'}</div>
+      </button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        title="Teste"
+        button1Title="Fechar"
+        button1Action={onClose}
+      >
+        <p>Fala tu</p>
+      </Modal>
+    </>
   );
 }
