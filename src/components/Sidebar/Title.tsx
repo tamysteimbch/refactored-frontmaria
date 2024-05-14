9b4844f49092298cd3018c465e4a1265b23da32f
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 10:28:07 -0300

    feat: Add Title component to Sidebar

---
+++ src/components/Sidebar/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div className="flex flex-col gap-2 text-center mt-12">
+      <h1 className="text-[3rem]">Maria</h1>
+      <p className="text-[#999999] text-sm mb-8">{`Bem vinda, professor(a) Flávia`}</p>
+    </div>
+  );
+}
