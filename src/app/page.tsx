ee027a78c5880a1e07751718a8b9fa50e5cf8733
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 10:27:55 -0300

    refactor: Update page component with Sidebar and MainContent components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client';
+import { useState } from 'react';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import MainContent from '@/components/MainContent';
+import { Mode } from '@/constants/sidebuttons';
 
 export default function Home() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+  const [mode, setMode] = useState<Mode>('class');
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
+    <div className="min-h-screen flex">
+      <Sidebar
+        isNavCollapsed={isNavCollapsed}
+        setIsNavCollapsed={setIsNavCollapsed}
+        setMode={setMode}
+      />
+      <MainContent
+        isNavCollapsed={isNavCollapsed}
+        setIsNavCollapsed={setIsNavCollapsed}
+        mode={mode}
+      />
+    </div>
   );
 }
