88cdb98f30c5deddfaab051aaeda6eb027d193ed
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 18:59:23 -0300

    feat: Update page component with mode setter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
         isNavCollapsed={isNavCollapsed}
         setIsNavCollapsed={setIsNavCollapsed}
         mode={mode}
+        setMode={setMode}
       />
     </div>
   );
