65984cdcc2755a24524f6f2b61bee2f54fc26d16
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 19:38:12 -0300

    refactor: Update ExamList component to use HeaderTitle and improve layout

--- src/components/Content/ExamList.tsx
+++ src/components/Content/ExamList.tsx
@@ -1,7 +1,21 @@
-export default function ExamList() {
+import HeaderTitle from './components/HeaderTitle';
+import { FaPlus } from 'react-icons/fa';
+import { Mode } from '@/constants/sidebuttons';
+
+interface ExamListProps {
+  setMode: (value: Mode) => void;
+}
+
+export default function ExamList({ setMode }: ExamListProps) {
   return (
     <div>
-      <h1>New ExamList</h1>
+      <HeaderTitle
+        title="Banco de questões"
+        description="Veja quais questões estão disponíveis para uso!"
+        buttonIcon={<FaPlus />}
+        buttonTitle="Adicionar nova questão"
+        setMode={setMode}
+      />
     </div>
   );
 }
