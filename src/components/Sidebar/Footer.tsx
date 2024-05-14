143e0e173019805b8a7c4851966552cb2a30cb72
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 10:38:07 -0300

    refactor: Update Footer component in Sidebar to use Next.js Image component

--- src/components/Sidebar/Footer.tsx
+++ src/components/Sidebar/Footer.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+import FlaviaImg from '@/assets/image.png';
+
 export default function Footer() {
   return (
-    <div>
-      <p>Imagem</p>
-      <p>Nome</p>
-      <button>...</button>
-    </div>
+    <button className="flex gap-4 m-8 items-center">
+      <Image src={FlaviaImg} alt="Flávia" width={100} height={100} />
+      <p className="text-2xl">Flávia</p>
+    </button>
   );
 }
