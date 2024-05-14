9f5d5a81916d512608287c3f02c6a4f9e9c68274
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 19:21:07 -0300

    feat: Update NewExam component with HeaderTitle and setMode prop

--- src/components/Content/NewExam.tsx
+++ src/components/Content/NewExam.tsx
@@ -1,7 +1,21 @@
-export default function NewExam() {
+import HeaderTitle from './components/HeaderTitle';
+import { FaPlus } from 'react-icons/fa';
+import { Mode } from '@/constants/sidebuttons';
+
+interface NewExamProps {
+  setMode: (value: Mode) => void;
+}
+
+export default function NewExam({ setMode }: NewExamProps) {
   return (
     <div>
-      <h1>New NewExam</h1>
+      <HeaderTitle
+        title="Criar nova prova"
+        description="Selecione as questões disponíveis abaixo"
+        buttonIcon={<FaPlus />}
+        buttonTitle="Adicionar nova questão"
+        setMode={setMode}
+      />
     </div>
   );
 }
