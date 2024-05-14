fecf78d4e4a16dfa2de0913c357413e9658275d8
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 19:11:50 -0300

    refactor: Update Classes component to use HeaderTitle and add new class button

--- src/components/Content/Classes.tsx
+++ src/components/Content/Classes.tsx
@@ -1,7 +1,15 @@
+import HeaderTitle from './components/HeaderTitle';
+import { FaPlus } from 'react-icons/fa';
+
 export default function Classes() {
   return (
     <div>
-      <h1>New Classes</h1>
+      <HeaderTitle
+        title="Turmas"
+        description="6 turmas para o ano de 2024"
+        buttonIcon={<FaPlus />}
+        buttonTitle="Adicionar nova questão"
+      />
     </div>
   );
 }
