585807360e06c21441f39333cebf2c65d0b8786f
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-14 19:25:45 -0300

    refactor: Update CreateExamBtn component to handle 'newExam' mode

--- src/components/Sidebar/CreateExamBtn.tsx
+++ src/components/Sidebar/CreateExamBtn.tsx
@@ -1,8 +1,16 @@
 import { FaPlus } from 'react-icons/fa';
+import { Mode } from '@/constants/sidebuttons';
 
-export default function CreateExamBtn() {
+interface CreateExamBtnProps {
+  setMode: (value: Mode) => void;
+}
+
+export default function CreateExamBtn({ setMode }: CreateExamBtnProps) {
   return (
-    <button className="grid grid-cols-[auto,1fr] bg-secondary p-4 rounded-lg gap-4 w-4/6 mx-auto hover:bg-tertiary items-center">
+    <button
+      className="grid grid-cols-[auto,1fr] bg-secondary p-4 rounded-lg gap-4 w-4/6 mx-auto hover:bg-tertiary items-center"
+      onClick={() => setMode('newExam')}
+    >
       <div className="text-xl">
         <FaPlus />
       </div>
