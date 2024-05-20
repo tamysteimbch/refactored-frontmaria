0509ff620702b40e7cfda59d041e61177cba8009
aysllanferreira <aysllan.ferreiraks@gmail.com>
2024-05-20 10:33:35 -0300

    feat: Filter exam content based on selected level and image filter

--- src/components/Content/NewExam.tsx
+++ src/components/Content/NewExam.tsx
@@ -1,16 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import HeaderTitle from './components/HeaderTitle';
 import { FaPlus } from 'react-icons/fa';
 import Cards from './components/Cards';
-import examContent from '@/constants/examContent';
-import { useState } from 'react';
+import examContent, { ExamContent } from '@/constants/examContent';
+import { useState, useEffect } from 'react';
 import { Mode } from '@/constants/sidebuttons';
 
-interface ExamContent {
+interface ExamContentProps {
   setMode: (mode: Mode) => void;
 }
 
-export default function NewExam({ setMode }: ExamContent) {
+export default function NewExam({ setMode }: ExamContentProps) {
   const [selecteds, setSelecteds] = useState<number[]>([]);
+  const [filteredExamContent, setFilteredExamContent] = useState<ExamContent[]>(examContent);
+  const [selectedLevel, setSelectedLevel] = useState<string>('all');
+  const [selectedImageFilter, setSelectedImageFilter] = useState<string>('all');
+
+  useEffect(() => {
+    filterExamContent();
+  }, [selectedLevel, selectedImageFilter]);
+
+  const filterExamContent = () => {
+    let filteredContent = examContent;
+
+    if (selectedLevel !== 'all') {
+      filteredContent = filteredContent.filter((content) => content.level === selectedLevel);
+    }
+
+    if (selectedImageFilter !== 'all') {
+      const booleanValue = selectedImageFilter === 'true';
+      filteredContent = filteredContent.filter((content) => content.hasImage === booleanValue);
+    }
+
+    setFilteredExamContent(filteredContent);
+  };
 
   return (
     <div>
@@ -21,7 +44,30 @@ export default function NewExam({ setMode }: ExamContent) {
         buttonTitle="Adicionar nova questão"
       />
 
-      <Cards examContent={examContent} setSelecteds={setSelecteds} selecteds={selecteds} />
+      <div className="flex items-center gap-4">
+        <select
+          className="border-[3px] rounded-lg p-2 mt-4"
+          onChange={(e) => setSelectedLevel(e.target.value)}
+          value={selectedLevel}
+        >
+          <option value="all">Todas as questões</option>
+          <option value="Fácil">Fáceis</option>
+          <option value="Médio">Médias</option>
+          <option value="Difícil">Difíceis</option>
+        </select>
+
+        <select
+          className="border-[3px] rounded-lg p-2 mt-4"
+          onChange={(e) => setSelectedImageFilter(e.target.value)}
+          value={selectedImageFilter}
+        >
+          <option value="all">Todas</option>
+          <option value="true">Com Imagem</option>
+          <option value="false">Sem Imagem</option>
+        </select>
+      </div>
+
+      <Cards examContent={filteredExamContent} setSelecteds={setSelecteds} selecteds={selecteds} />
 
       <div className="flex justify-between px-12 items-center mt-6">
         <button
--- src/components/Content/components/Cards.tsx
+++ src/components/Content/components/Cards.tsx
@@ -13,7 +13,7 @@ interface CardsProps {
 export default function Cards({ cardContent, examContent, setSelecteds, selecteds }: CardsProps) {
   return (
     <div
-      className="custom-scrollbar grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 mt-24 p-4 md:overflow-auto"
+      className="custom-scrollbar grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 mt-8 p-4 md:overflow-auto"
       style={{ maxHeight: 'calc(100vh - 24rem)' }}
     >
       {cardContent && cardContent.map((card: CardContent) => <Card key={card.title} {...card} />)}
